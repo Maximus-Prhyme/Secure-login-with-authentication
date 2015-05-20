feca5aefe346c3f2d0a21fac8dd7f1bef3b1cf6d
Maximo Rosario <xam527@gmail.com>
2015-05-20 11:30:16 -0400

    actually saved the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+
   def new
     @users = User.new
   end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      redirect_to root_path, notice: "created user"
+    else render action: 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.
+    require( :user).
+    permit(:username, :password, :password_confirmation)
+  end
+
 end
